038c24ce9da6f42b8d32d95960e37d784a4e40aa
rohitsengar001 <rohitsengar001@gmail.com>
2021-05-15 03:43:24 +0530

    content and layout component created

--- src/app/modules/webview/webview.module.ts
+++ src/app/modules/webview/webview.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ContentComponent } from './content/content.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, LayoutComponent, ContentComponent],
   imports: [
     CommonModule
   ]
