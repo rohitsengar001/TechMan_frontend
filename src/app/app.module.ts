27d97f661fd12f3dbe6f5d09de0467cab3fa4afe
Emir-Tarun16 <naman.bkarogyam@gmail.com>
2021-05-18 03:08:00 +0530

    about us page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
